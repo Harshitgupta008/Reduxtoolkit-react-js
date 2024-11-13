9b578861a92e80dada46ea7568e347cb27d14a25
harshit gupta <7317500h.harsh@gmail.com>
2024-11-13 19:29:35 +0530

    update list and add remove funcationalty

--- src/component/UserList.jsx
+++ src/component/UserList.jsx
@@ -1,19 +1,33 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { RemoveData } from "../redux/features/todos/todoSlice";
 
 const UserList = () => {
-    const todo = useSelector((state) => state.todo.todo)
+    const todo = useSelector((state) => state.todo.todo);
+    const dispatch = useDispatch();
+
+    const DeleteData = (id)=>{
+        dispatch(RemoveData(id));
+    }
     return (
         <>
             <div className="userlist-container">
                 {
                     Array.isArray(todo) && todo.length > 0 ? (
                         todo.map((item, id) => (
+
                             <div key={id} className="userlist-card">
                                 <div className="userlist-name">
                                     <h2>::</h2>
                                     <h4>{item.name}</h4>
                                 </div>
                                 <h2>&#8942;</h2>
+                                <div className="userlist-hidecard">
+                                    <ul>
+                                        <li>View</li>
+                                        <li onClick={()=>DeleteData(id)}>Delete</li>
+                                        <li>Update</li>
+                                    </ul>
+                                </div>
                             </div>
                         ))
                     ) : (
